c70d605fe410a1cc640a06c2c9b047fe6e28c6a1
Gabriel Miranda <GabrielMiranda@quadraengenharia.local>
2025-07-10 10:34:34 -0300

    nova atualização7

--- bot/app.py
+++ bot/app.py
@@ -84,7 +84,7 @@ st.markdown("""
 
     /* Centraliza melhor o input, sem causar rolagem */
     div[data-testid="stForm"] {
-        margin-top: 20vh;
+        margin-top: 10vh;
         margin-bottom: 2vh;
     }
     </style>
