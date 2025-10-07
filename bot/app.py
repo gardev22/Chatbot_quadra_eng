d42021ce548aef9f6cbc6847b9a6342557a31b13
gardev22 <gabrielfm99@yahoo.com.br>
2025-10-07 18:49:43 -0300

    ajustes_finais1

--- bot/app.py
+++ bot/app.py
@@ -202,7 +202,7 @@ div[data-testid="stAppViewContainer"]{ margin-left: var(--sidebar-w) !important;
 
 /* remove o respiro padrão do contêiner interno da sidebar do Streamlit */
 div[data-testid="stSidebarContent"]{
-  padding-top: 4 !important;
+  padding-top: 10 !important;
 }
 
 /* garante que o primeiro bloco não reintroduza espaçamento */
